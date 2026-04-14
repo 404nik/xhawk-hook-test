5bc84acee4507ba151df59241d85b0ed84cb2e5b
404nik <nikhil.unavekar.07@gmail.com>
2026-04-14 17:02:53 +0530

    feat: add hello_niks function

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
+
 def say_hello(name):
     print(f"Hello, {name}!")
+
+def hello_niks():
+    print("Hello, Niks!")
 
 if __name__ == "__main__":
     print("Hello, World!")
     say_hello("Nikhil")
+    hello_niks()
